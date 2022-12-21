973e52ba27cb3f1b71f295df7b3702e83682f07b
Skydodle <skydodle@gmail.com>
2022-12-21 23:28:11 +0800

    Added ts decorators to game class

--- src/game/entities/game.entity.ts
+++ src/game/entities/game.entity.ts
@@ -1,8 +1,33 @@
+import 'reflect-metadata';
 import { ObjectType, Field, Int } from '@nestjs/graphql';
 
 @ObjectType()
 export class Game {
-  @Field(() => Int, { description: 'Example field (placeholder)' })
-  exampleField: number;
+  @Field((type) => Int)
+  id: number;
+
+  @Field((type) => Date)
+  createdAt: Date;
+
+  @Field((type) => Date)
+  updatedAt: Date;
+
+  @Field((type) => Int)
+  player1Id: number;
+
+  @Field((type) => Int)
+  player2Id: number;
+
+  @Field((type) => Boolean)
+  isActive: boolean;
+
+  @Field((type) => Int, { nullable: true })
+  gameWinner: number;
+
+  @Field((type) => Int, { nullable: true })
+  player1Hits: number;
+
+  @Field((type) => Int, { nullable: true })
+  player2Hits: number;
 }
 // TODO FILL THIS OUT TO GENERATE CODE FIRST GRAPHQL SCHEMA
